1a886c0fcbeeb9817347372e10970f36d2d5372c
josphh <28985840+josphh@users.noreply.github.com>
2020-09-16 20:07:06 +0100

    Update main.py

--- main/main.py
+++ main/main.py
@@ -3,6 +3,35 @@ import os
 import json
 from discord.ext import commands
 
+#allows user to change bot prefix
+def get_prefix(client, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes =json.load(f)
+
+    return prefixes[str(message.guild.id)]
+
+@client.event
+async def on_guild_join(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes =json.load(f)
+
+    prefixes[str(guild.id)] = 'f.'
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f indent=4)
+
+@client.event
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes =json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f indent=4)
+
+
+client = commands.Bot(command_prefix = get_prefix)
 
 #error message if a command isn't found
 @client.event
