1b69228573a90623d580570156424ce5678ce1e9
Daniel Thwaites <danthwaites30@btinternet.com>
2020-09-18 17:28:35 +0100

    Fix status cog

--- main/cogs/status.py
+++ main/cogs/status.py
@@ -1,20 +1,20 @@
 import discord
-from discord.ext import tasks
+from discord.ext import commands, tasks
 from itertools import cycle
 
 status = cycle(['f.help', '\*ribbit\*', 'uwu froggy go croak croak'])
 
-class Status_cycle(tasks.Cog):
+class Status_cycle(commands.Cog):
 
 	def __init__(self, client):
 		self.client = client
 
 
 
-@tasks.loop(seconds=10)
-async def change_status(self):
-	await self.client.change_presence(status=discord.Status.idle, activity=discord.Game(status))
+	@tasks.loop(seconds=10)
+	async def change_status(self):
+		await self.client.change_presence(status=discord.Status.idle, activity=discord.Game(status))
 
 
 def setup(client):
-	client.add_cog(Greet(client))
+	client.add_cog(Status_cycle(client))
