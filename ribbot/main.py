69c2eec3efd87a2ae324e37afffe8faa6a8c157e
Daniel Thwaites <danthwaites30@btinternet.com>
2020-10-22 18:20:38 +0100

    Set up Pylint (#11)
    * Set up Pylint to run on GitHub Actions
    * Fix existing problems

--- ribbot/main.py
+++ ribbot/main.py
@@ -2,14 +2,13 @@ import json
 import os
 import sys
 
-import discord
 from discord.ext import commands
 
 
-# loads the prefix from the json file and allows the user to change it
-def get_prefix(client, message):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
+def get_prefix(_, message):
+    """Load the prefix from prefixes.json."""
+    with open("prefixes.json", "r") as prefixes_file:
+        prefixes = json.load(prefixes_file)
 
     return prefixes[str(message.guild.id)]
 
@@ -17,53 +16,56 @@ def get_prefix(client, message):
 client = commands.Bot(command_prefix=get_prefix)
 
 
-## sets default value to '.f'
 @client.event
 async def on_guild_join(guild):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
+    """Set default prefix to '.f'."""
+    with open("prefixes.json", "r") as prefixes_file:
+        prefixes = json.load(prefixes_file)
 
     prefixes[str(guild.id)] = "f."
 
-    with open("prefixes.json", "w") as f:
-        json.dump(prefixes, f)
+    with open("prefixes.json", "w") as prefixes_file:
+        json.dump(prefixes, prefixes_file)
 
 
 @client.event
 async def on_guild_remove(guild):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
+    """Delete prefix setting when the bot is removed from a guild."""
+    with open("prefixes.json", "r") as prefixes_file:
+        prefixes = json.load(prefixes_file)
 
     prefixes.pop(str(guild.id))
 
-    with open("prefixes.json", "w") as f:
-        json.dump(prefixes, f)
+    with open("prefixes.json", "w") as prefixes_file:
+        json.dump(prefixes, prefixes_file)
 
 
 @client.command()
 async def changeprefix(ctx, prefix):
-    with open("prefixes.json", "r") as f:
-        prefixes = json.load(f)
+    """Change prefix for the given context."""
+    with open("prefixes.json", "r") as prefixes_file:
+        prefixes = json.load(prefixes_file)
 
     prefixes[str(ctx.guild.id)] = prefix
 
-    with open("prefixes.json", "w") as f:
-        json.dump(prefixes, f)
+    with open("prefixes.json", "w") as prefixes_file:
+        json.dump(prefixes, prefixes_file)
 
-    await ctx.send(f"Prefix changed to: " + prefix)
+    await ctx.send("Prefix changed to: " + prefix)
 
 
-# error message if a command isn't found
 @client.event
 async def on_command_error(ctx, error):
+    """Send an error message if a command isn't found."""
     if isinstance(error, commands.CommandNotFound):
-        await ctx.send("\*confused croak\*. Invalid command used")
+        await ctx.send("\\*confused croak\\*. Invalid command used")
 
 
 # commands to load and unload the cogs in ./cogs
 @client.command()
 @commands.is_owner()
 async def load(ctx, extension):
+    """Load a cog from ./cogs."""
     client.load_extension(f"cogs.{extension}")
     await ctx.send("*Cog Loaded*")
 
@@ -71,6 +73,7 @@ async def load(ctx, extension):
 @client.command()
 @commands.is_owner()
 async def unload(ctx, extension):
+    """Unload a cog from ./cogs."""
     client.unload_extension(f"cogs.{extension}")
     await ctx.send("*Cog Unloaded*")
 
@@ -78,6 +81,7 @@ async def unload(ctx, extension):
 @client.command()
 @commands.is_owner()
 async def reload(ctx, extension):
+    """Reload a cog from ./cogs."""
     client.reload_extension(f"cogs.{extension}")
     await ctx.send("*Cog Reloaded*")
 
@@ -91,4 +95,4 @@ try:
     client.run(os.environ["RIBBOT_DISCORD_TOKEN"])
 except KeyError:
     print("Please set the environment variable RIBBOT_DISCORD_TOKEN.", file=sys.stderr)
-    exit(2)
+    sys.exit(2)
